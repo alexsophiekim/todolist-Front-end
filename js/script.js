b32a7a6df6393dcaacdef174bbd8deaae31e87a2
Sophie Kim <270010565@bun2-206-25.acg.ac.nz>
2019-08-28 12:35:16 +1200

    ajax post working / making edit function

--- js/script.js
+++ js/script.js
@@ -15,35 +15,7 @@ $.ajax({
     console.log('Something went wrong');
   }
 });
-//
-// getList = () => {
-//   $.ajax({
-//     url:`${serverURL}:${serverPort}/allLists`,
-//     type: 'GET',
-//     dataType: 'json',
-//     success: function(list){
-//       console.log(list);
-//       for (var i = 0; i < list.length; i++) {
-//         $("#listBox").append(`
-//           <li class="list-group-item d-flex justify-content-between align-items-center">
-//              <div class="listName">${list[i].name}
-//              </div>
-//              <div class="dueDate">
-//                ${list[i].dueDate}
-//              </div>
-//              <div>
-//                <button class="btn btn-warning editBtn">Edit</button>
-//                <button class="btn btn-danger deleteBtn">Delete</button>
-//              </div>
-//            </li>`);
-//       }
-//     },
-//     error: function(err){
-//       console.log(err);
-//       console.log('Something went wrong');
-//     }
-//   });
-// }
+
 $("#addBtn").click(function(){
   event.preventDefault();
   let listItem = $('#listName').val();
@@ -52,6 +24,56 @@ $("#addBtn").click(function(){
   if ((listName.lengh === 0)|| (dueDate.length === 0)) {
     console.log('add your to do list');
   } else {
+    if(editing === true){
+      const id = $("#listID").val();
+      $.ajax({
+        url:`${serverURL}:${serverPort}/editList/${id}`,
+        type:'PATCH',
+        data: {
+          list:listItem,
+          dueDate: dueDate
+        },
+        success: function(result){
+          $("#listName").val(null);
+          $("#dueDate").val(null);
+          $("#listID").val(null);
+          $("#addBtn").text('EDIT').removeClass('btn-success');
+          $("#heading").text('Edit your to do list');
+          editing = false;
+        },
+        error: function(err){
+          console.log(err);
+          console.log('Something went wrong');
+        }
+      })  //ajax end
+  } else {
     console.log(`${listItem} - ${dueDate}`);
-  }
-})
+    $.ajax({
+      url:`${serverURL}:${serverPort}/list`,
+      type:'POST',
+      data:{
+        list:listItem,
+        dueDate: dueDate
+      },
+      success:function(result){
+        $("#listBox").append(`
+          <li class="list-group-item d-flex justify-content-between align-items-center">
+             <div class="listName">${listItem}
+             </div>
+             <div class="dueDate">
+               ${dueDate}
+             </div>
+             <div>
+               <button class="btn btn-warning editBtn">Edit</button>
+               <button class="btn btn-danger deleteBtn">Delete</button>
+             </div>
+           </li>`);
+      },
+      error:function(err){
+        console.log(err);
+        console.log('Something went wrong');
+      }
+    }) //ajax done
+  } //else done
+ }
+}); //addBTN end
